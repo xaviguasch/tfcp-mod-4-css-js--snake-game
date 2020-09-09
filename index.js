be611dc14a4ab5576653677230c5f3fe31b031e4
xaviguasch <xaviguasch@gmail.com>
2020-09-09 13:47:25 +0200

    removed snake's tail with pop()

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 let squares = []
-let currentSnake = [0, 1, 2]
+let currentSnake = [2, 1, 0]
 
 function createGrid() {
   //create 100 of these elements with a for loop
@@ -20,3 +20,15 @@ function createGrid() {
 createGrid()
 
 currentSnake.forEach((index) => squares[index].classList.add('snake'))
+
+function move() {
+  //remove last element from our currentSnake array
+  const tail = currentSnake.pop()
+
+  //remove styling from last element
+  squares[tail].classList.remove('snake')
+  //add sqaure in direction we are heading
+
+  //add styling so we can see it
+}
+move()
