3f28c1ea5d5d50144382cb4b5be1782e5f7773f8
xaviguasch <xaviguasch@gmail.com>
2020-09-09 12:39:43 +0200

    added squares to the grid via js

---
+++ index.js
@@ -0,0 +1,19 @@
+const grid = document.querySelector('.grid')
+const startButton = document.getElementById('start')
+const score = document.getElementById('score')
+
+let squares = []
+
+function createGrid() {
+  for (let i = 0; i < 100; i++) {
+    const square = document.createElement('div')
+
+    square.classList.add('square')
+
+    grid.appendChild(square)
+
+    squares.push(square)
+  }
+}
+
+createGrid()
