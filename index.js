925274fb1684bcae5e72b45f3ef21eef4cb55357
xaviguasch <xaviguasch@gmail.com>
2020-09-09 12:58:20 +0200

    drawn the snake on the grid

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
-
 let squares = []
+let currentSnake = [0, 1, 2]
 
 function createGrid() {
+  //create 100 of these elements with a for loop
   for (let i = 0; i < 100; i++) {
+    //create element
     const square = document.createElement('div')
-
+    //add styling to the element
     square.classList.add('square')
-
+    //put the element into our grid
     grid.appendChild(square)
-
+    //push it into a new squares array
     squares.push(square)
   }
 }
-
 createGrid()
+
+currentSnake.forEach((index) => squares[index].classList.add('snake'))
