bebe225c9a917752d935ee8d5a56af0c622807db
xaviguasch <xaviguasch@gmail.com>
2020-09-09 19:13:06 +0200

    changed snake's direction

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const score = document.getElementById('score')
 let squares = []
 let currentSnake = [2, 1, 0]
 let direction = 1
+let width = 10
 
 function createGrid() {
   //create 100 of these elements with a for loop
@@ -35,17 +36,20 @@ function move() {
 }
 move()
 
-// let timerId = setInterval(move, 1000)
+let timerId = setInterval(move, 1000)
 
 function control(e) {
   if (e.keyCode === 39) {
-    console.log('right pressed')
+    direction = 1
   } else if (e.keyCode === 38) {
     console.log('up pressed')
+    direction = -width
   } else if (e.keyCode === 37) {
-    console.log('left pressed')
+    direction = -1
   } else if (e.keyCode === 40) {
     console.log('down pressed')
+    direction = +width
   }
 }
+
 document.addEventListener('keyup', control)
